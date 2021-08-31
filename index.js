5a4d006a9cd940b1cb642e8a406244e44d87f3ae
Jason Moritz <moritz.jason@gmail.com>
2021-08-30 19:26:05 -0500

    flexbox added / details still not working

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-// Grab drop-downs and button for API requests
-
-
-
+// Set base url
 
 const baseURL = "http://ergast.com/api/f1/";
 
@@ -90,7 +87,7 @@ dropDownYearHTML.addEventListener("change", dropDownCircuitsAPI);
 
 // **********RACE BUTTON**********
 
-const raceButtonHTML = document.querySelector("#race-button");
+const raceButtonHTML = document.querySelector("#button");
 
 // Need axios request based on year and circuit drop-down selection
 
@@ -106,7 +103,7 @@ async function raceButtonAPI(event) {
         // console.log(resultInfo);
 
 
-        displayResults(`${resultArr[0].Constructor.name} ${resultArr[0].Driver.givenName} ${resultArr[0].Driver.familyName}`, `${resultArr[1].Constructor.name} ${resultArr[1].Driver.givenName} ${resultArr[1].Driver.familyName}`, `${resultArr[2].Constructor.name} ${resultArr[2].Driver.givenName} ${resultArr[2].Driver.familyName}`)
+        displayResults(`${resultArr[0].Constructor.name} --- ${resultArr[0].Driver.givenName} ${resultArr[0].Driver.familyName}`, `${resultArr[1].Constructor.name} --- ${resultArr[1].Driver.givenName} ${resultArr[1].Driver.familyName}`, `${resultArr[2].Constructor.name} --- ${resultArr[2].Driver.givenName} ${resultArr[2].Driver.familyName}`)
 
 
     } catch(error) {
@@ -154,16 +151,51 @@ function displayResults(result1, result2, result3) {
     secondPlaceResultsHTML.append(secondPlace);
     thirdPlaceResultsHTML.append(thirdPlace);
 
-
-    allResultsHTML.forEach((newResult) => {
-        let detailsButton = document.createElement("button");
-        detailsButton.innerText = "Details";
-        newResult.append(detailsButton);
-        detailsButton.addEventListener("click", (e) => {
-            displayDetailsAPI(e)   
-        })
-    })
+    addButtons();
+    // allResultsHTML.forEach(() => {
+    //     let detailsButton = document.createElement("button");
+    //     detailsButton.innerText = "Details";
+    // })
 }
+
+
+
+
+function addButtons() {
+    let firstPlaceButton = document.createElement("button");
+    let secondPlaceButton = document.createElement("button");
+    let thirdPlaceButton = document.createElement("button");
+
+    firstPlaceButton.innerText = "Details";
+    secondPlaceButton.innerText = "Details";
+    thirdPlaceButton.innerText = "Details";
+
+
+    firstPlaceResultsHTML.append(firstPlaceButton);
+    secondPlaceResultsHTML.append(secondPlaceButton);
+    thirdPlaceResultsHTML.append(thirdPlaceButton);
+
+    let buttonsAll = document.querySelectorAll("button");
+    console.log(buttonsAll);
+
+    firstPlaceButton.setAttribute("id", 0);
+    secondPlaceButton.setAttribute("id", 1);
+    thirdPlaceButton.setAttribute("id", 2);
+
+   buttonsAll.forEach((button) => {
+       button.addEventListener("click", (e) => {
+           displayDetailsAPI(e.target.id);
+        })
+    });
+}
+
+    // allResultsHTML.forEach((newResult) => {
+    //     let detailsButton = document.createElement("button");
+    //     detailsButton.innerText = "Details";
+    //     newResult.append(detailsButton);
+        
+    // })
+
 
 
 // **********DISPLAY DETAILS**********
@@ -171,14 +203,21 @@ function displayResults(result1, result2, result3) {
 // Added event listener in the display results to each details button
 // Need async function to re-pull results info
 // Need display
-async function displayDetailsAPI(e) {
+async function displayDetailsAPI(id) {
+    console.log(id);
+    
     try {
         let response = await axios.get(`${baseURL}${dropDownYearHTML.value}/${dropDownCircuitsHTML.value}/results.json`)
         let detailsArr = response.data.MRData.RaceTable.Races[0].Results;
-
-
-        displayDetails(`Total Time - ${detailsArr[e].Time.time}  Fastest Lap - ${detailsArr[e].FastestLap.Time.time}`)
-
+        console.log(detailsArr);
+        console.log(detailsArr[id].Time.time);
+        
+        if (detailsArr[id].FastestLap.Time.time) {
+            displayDetails(`Overall time --- ${detailsArr[id]?.Time.time}`, `Fastest Lap --- ${detailsArr[id]?.FastestLap.Time.time}`, `Average Speed --- ${detailsArr[id]?.FastestLap.AverageSpeed.speed}`, id)
+        
+        }    else {
+            displayDetails(`fastest lap unknown`)
+        }
 
 
         console.log(detailsArr);
@@ -190,10 +229,31 @@ async function displayDetailsAPI(e) {
 
 // Need function to append details in replace of race results
 
-function displayDetails (detail) {
-    let newDetails = document.createElement("h5");
-
+function displayDetails (detail1, detail2, detail3, id) {
+    if (id === 0) {
+        firstPlaceResultsHTML.innerText = "";
     
-    newDetails.innerText = detail;
-    this.append(newDetails);
+    
+        let newDetails = document.createElement("h5");
+        newDetails.innerText = detail1;
+        firstPlaceResultsHTML.append(newDetails);
+    
+
+    } else if (id === 1) {
+        secondPlaceResultsHTML.innerText = "";
+        
+        
+        let newDetails = document.createElement("h5");
+        newDetails.innerText = detail2;
+        secondPlaceResultsHTML.append(newDetails);
+    
+
+    } else if (id === 2) {
+        thirdPlaceResultsHTML.innerText = "";
+        
+        
+        let newDetails = document.createElement("h5");
+        newDetails.innerText = detail3;
+        thirdPlaceResultsHTML.append(newDetails);
+    }
 }
